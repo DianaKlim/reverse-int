61bf516163143f569ab80b5ae12ae795e9f41fec
Diana <diana.klimsmm@gmailcom>
2022-07-08 23:59:56 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+        // const reverse = (n) => {
+    let nPositive = Math.abs(n);
+    let lineNumber = String(nPositive);
+    let resultReverse = String();
+    for (i = lineNumber.length - 1; i >= 0; i--) {
+        resultReverse += lineNumber[i]
+        }
+        return resultReverse;
+        // console.log(resultReverse);
+  };
+    //   reverse('-261');
+    //   reverse('451');
+
+
+//костыльный вариант
+
+// module.exports = function reverse(n) {
+//     const ns = String(Math.abs(n));
+//     const positionNumber1 = ns.length-1;
+//     const positionNumber2 = ns.length-2;
+//     const positionNumberend = ns.length-3;
+//     const result2 = Number(ns[positionNumber1] + ns[positionNumber2] + ns[positionNumberend]);
+//     return result2
+ 
+//       };
